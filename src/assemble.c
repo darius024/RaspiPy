03b9f60ecece11d6d04f8fda2280cf2d4cd40913
Yechan Yun <yechan.yun23@imperial.ac.uk>
2024-05-24 23:23:12 +0100

    Can input two filenames. Outputs error if no two arguments

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+	if (argc >= 3) {
+		printf("%s\n", argv[1]);
+		printf("%s\n", argv[2]);
+	}	
+	else {
+		fprintf(stderr, "More arguments needed\n");
+	}
+	return EXIT_SUCCESS;
 }

4094a282a4176b6f871e0f46d4aebafe1202f5e2
Yechan Yun <yechan.yun23@imperial.ac.uk>
2024-05-25 13:48:45 +0100

    Defined parserRouter, which routes an instruction to appropiate parser e.g. branchParser. Provided templates for each parser

--- src/assemble.c
+++ src/assemble.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define MAXCOMPONENTS 4
 #define SPACE " "
+#define OPCODE 0
 
 /*
 movz x1,#1
@@ -10,9 +11,28 @@ add x2,x1,#2
 and x0, x0, x0
 */
 
-void parserRouter(char *instruction) {
-	int numComponents;
-	char ** components = decompose(instruction, &numComponents);
+extern char **decompose(char *instruction, int *numComponents);
+extern void parserRouter(char *instruction);
+
+extern void branchParser(char **components, int numComponents);
+extern void loadStoreParser(char **components, int numComponents);
+extern void specialParser(char **components, int numComponents);
+extern void dpParser(char **components, int numComponents);
+
+void branchParser(char **components, int numComponents){
+	return;
+}
+
+void loadStoreParser(char **components, int numComponents){
+	return;
+}
+
+void specialParser(char **components, int numComponents){
+	return;
+}
+
+void dpParser(char **components, int numComponents){
+	return;
 }
 
 char **decompose(char *instruction, int *numComponents) { // returns array of each component (string) of an instruction
@@ -36,32 +56,21 @@ char **decompose(char *instruction, int *numComponents) { // returns array of ea
 
 }
 
-int main(int argc, char **argv) {
-	if (argc >= 3) {
-		printf("%s\n", argv[1]);
-		printf("%s\n", argv[2]);
-	}	
-	else {
-		fprintf(stderr, "More arguments needed\n");
+void parserRouter(char *instruction) {
+	if (*instruction == '\n') {
+		return;
 	}
 	int numComponents;
-	char str[] = "hello there";
-	char **a = decompose(str, &numComponents);
-	printf("components: %d\n", numComponents);
-	for (int i = 0 ; i < numComponents ; i++) {
-		printf("%s\n", a[i]);
-	}
-
-	return EXIT_SUCCESS;
-}
-
-/*
-void comps(char *instruction) {
-	
-	char *comp = strtok(instruction, SPACE);
-	while (comp != NULL) {
-		printf("%s\n", comp);
-		comp = strtok(NULL, SPACE);
+	char **components = decompose(instruction, &numComponents);
+	char *opcode = components[0];
+	if (strcmp(opcode, "b") == 0 || strcmp(opcode, "b.cond") || strcmp(opcode, "br") == 0) {
+		branchParser(components, numComponents);
+	} else if (strcmp(opcode, "str") == 0 || strcmp(opcode, "ldr") == 0) {
+		loadStoreParser(components, numComponents);
+	} else if (strcmp(opcode, ".int") == 0) {
+		specialParser(components, numComponents);
+	} else {
+		dpParser(components, numComponents);
 	}
 }
 
@@ -72,12 +81,11 @@ int main(int argc, char **argv) {
 	}	
 	else {
 		fprintf(stderr, "More arguments needed\n");
-	}
-	char str[] = "hello there";
-	comps(str);
-	return 0;
+	}	
+	// while loop here to fetch each line of assembly code. Each line is then fed into routeParser
+	
+	return EXIT_SUCCESS;
 }
-*/
 
 
 
